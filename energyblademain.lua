fbaba7065746ccea23392c952c6acd34ece9fba7
GoHamza <82454201+GoHamza@users.noreply.github.com>
2023-02-19 12:32:25 +0500

    Update energyblademain.lua

--- energyblademain.lua
+++ energyblademain.lua
@@ -1,126 +1,142 @@
-
-local cevent = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
-local function chat(ass)
-	cevent:FireServer(ass,"All")
-end
-local plr = game:GetService("Players").LocalPlayer
-local value = ""
-local function chr()
-	return plr.Character
-end
-local function hum()
-	return chr():FindFirstChildWhichIsA("Humanoid")
+-- text color is 76.5, 255, 76.5
+-- background is 0, 0, 0
+	
+		
+if not game:IsLoaded() then
+	game.Loaded:Wait()
 end
 
-local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
-
-local X = Material.Load({
-	Title = " EnergyBlade",
-	Style = 4,
-	SizeX = 550,
-	SizeY = 400,
-	Theme = "Dark",
-})
-
-local Y = X.New({
-	Title = "Character"
-})
-
---[[
-local B = Y.Toggle({
-	Text = "Uppercase",
-	Callback = function(Value)
-		uppercase = Value
-	end,
-	Enabled = false
-})
-]]
-
-local A = Y.Button({
-	Text = "EnergyBlade is in beta, so expect bugs.",
-})
-local H = Y.TextField({
-	Text = "Value",
-	Callback = function(Value)
-		value = Value
-	end,
-})
-
-local A = Y.Button({
-	Text = "WalkSpeed",
-	Callback = function()
-		hum().WalkSpeed = value
-	end,
-})
-
-local A = Y.Button({
-	Text = "Reset WalkSpeed",
-	Callback = function()
-		hum().WalkSpeed = 16
-	end,
-})
-
-local A = Y.Button({
-	Text = "JumpPower",
-	Callback = function()
-		hum().JumpPower = value
-	end,
-})
-
-local A = Y.Button({
-	Text = "Reset JumpPower",
-	Callback = function()
-		hum().JumpPower = 50
-	end,
-})
-
-local A = Y.Button({
-	Text = "HipHeight",
-	Callback = function()
-		hum().HipHeight = value
-	end,
-})
-
-local A = Y.Button({
-	Text = "Reset HipHeight",
-	Callback = function()
-		hum().HipHeight = 0
-	end,
-})
-
-local B = Y.Toggle({
-	Text = "Teleport Fly",
-	Callback = function(Value)
-		if Value == true then
-			local UIS = game:GetService('UserInputService')
-local RunService = game:GetService('RunService')
-local Keys, v3, cf = Enum.KeyCode, Vector3.new(), CFrame.new()
-SPI = 2
-plr.Character.HumanoidRootPart.Anchored = true
-Con = RunService.Heartbeat:Connect(function()
-	local Camera = workspace.CurrentCamera
-	plr.Character.HumanoidRootPart.CFrame = CFrame.new(plr.Character.HumanoidRootPart.Position, plr.Character.HumanoidRootPart.Position + Camera.CFrame.LookVector) * (UIS:GetFocusedTextBox() and cf or CFrame.new((UIS:IsKeyDown(Keys.D) and SPI) or (UIS:IsKeyDown(Keys.A) and -SPI) or 0, (UIS:IsKeyDown(Keys.E) and SPI / 2) or (UIS:IsKeyDown(Keys.Q) and -SPI / 2) or 0, (UIS:IsKeyDown(Keys.S) and SPI) or (UIS:IsKeyDown(Keys.W) and -SPI) or 0))
-end)
-		else
-		if Con then
-	Con:Disconnect()
+if gethui():FindFirstChild("EnergyBlade") then
+	gethui().EnergyBlade:Destroy()
 end
-plr.Character.HumanoidRootPart.Anchored = false
-		end
-	end,
-	Menu = {
-        ["What is this?"] = function(self)
-            X.Banner({
-                Text = "Others cannot see this type of fly. To them it looks like teleporting when you disable it."
-            })
-        end
-    },
-	Enabled = false
-})
 
-local A = Y.Button({
-	Text = "Infinite Yield commands by Edge",
-	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
-	end,
-})
+-- init
+player = game.Players.LocalPlayer
+mouse = player:GetMouse()
+
+-- services
+input = game:GetService("UserInputService")
+run = game:GetService("RunService")
+tween = game:GetService("TweenService")
+tweeninfo = TweenInfo.new
+
+-- additional
+utility = {}
+
+-- themes
+objects = {}
+themes = {
+	Background = Color3.fromRGB(24, 24, 24), 
+	Glow = Color3.new(0.3, 1, 0.3), 
+	Accent = Color3.fromRGB(10, 10, 10), 
+	LightContrast = Color3.fromRGB(20, 20, 20), 
+	DarkContrast = Color3.fromRGB(14, 14, 14),  
+	TextColor = Color3.new(0.3, 1, 0.3)
+}
+
+lib = loadstring(game:HttpGet(string.reverse("eurt=war?detidexynev/niam/bolb/tessab-tessa/azmaHoG/moc.buhtig//:sptth")))()
+
+local u2 = UDim2.new
+	
+function newinst(class, parent, name) -- better implementation of roblox instancing
+   local inst = Instance.new(class)
+
+   if parent == 1 then
+      inst.Parent = gethui()
+   else
+      inst.Parent = parent
+   end
+
+   if name then
+      inst.Name = name
+   end
+
+   return inst
+end
+
+local sg = newinst("ScreenGui", 1)
+local frame = newinst("Frame",sg)
+local txt1 = newinst("TextLabel",frame)
+local txt2 = newinst("TextLabel",frame)
+local corner = newinst("UICorner", frame)
+
+frame.AnchorPoint=Vector2.new(.5,.5)
+frame.Position = u2(0.5,0,0.5,0)
+frame.Size = u2(0,500,0,0)
+frame.ClipsDescendants = true
+frame.BorderSizePixel = 0
+frame.BackgroundColor3 = Color3.new(0,0,0)
+
+corner.CornerRadius = UDim.new(0,10)
+
+txt1.AnchorPoint=Vector2.new(.5,.5)
+txt1.Position = u2(0.5,0,0.5,0)
+txt1.Size = u2(1,0,1,0)
+txt1.BackgroundTransparency = 1
+txt1.Text="EnergyBlade"
+txt1.TextColor3 = Color3.new(0.3,1,0.3)
+txt1.Font = 20
+txt1.TextSize = 40
+
+txt2.AnchorPoint=Vector2.new(.5,.5)
+txt2.Position = u2(0.5,0,1.5,0)
+txt2.Size = u2(1,0,1,0)
+txt2.BackgroundTransparency = 1
+txt2.Text="Powered by Krnl. \nPress Right CTRL to open."
+txt2.TextColor3 = Color3.new(0,0,0)
+txt2.Font = 17
+txt2.TextSize = 20
+txt2.TextWrapped = true
+
+frame:TweenSize(
+	UDim2.new(0,500,0,70), 
+	Enum.EasingDirection.InOut,
+	Enum.EasingStyle.Sine, 
+	0.3
+)
+
+wait(2)
+
+txt1:TweenPosition(
+	UDim2.new(0.5,0,-0.5,0), 
+	Enum.EasingDirection.InOut,
+	Enum.EasingStyle.Quint, 
+	1
+)
+
+txt2:TweenPosition(
+	UDim2.new(0.5,0,0.5,0), 
+	Enum.EasingDirection.InOut,
+	Enum.EasingStyle.Quint, 
+	1
+)
+for i = 0, 23 do
+	task.wait()
+	frame.BackgroundColor3 = frame.BackgroundColor3:lerp(Color3.new(0.3,1,0.3),0.2)
+end
+wait(4)
+
+frame:TweenSize(
+	UDim2.new(0,500,0,-0), 
+	Enum.EasingDirection.In,
+	Enum.EasingStyle.Sine, 
+	0.3
+)
+for i = 0, 23 do
+	task.wait()
+	frame.BackgroundColor3 = frame.BackgroundColor3:lerp(Color3.new(0,0,0),0.2)
+end
+game:service'Debris':AddItem(sg, 3)
+
+local main = lib.new("EnergyBlade")
+
+local pg1 = main:addPage("Welcome")
+
+-- // Sections for Test Page
+pg1:addSection("Welcome to EnergyBlade!")
+pg1:addSection("Made by windowschips, for individual use only.")
+pg1:addSection("Please abide by Krnl's Terms of Service, and have fun!")
+local crOdits = pg1:addSection("Credits:")
+crOdits:addButton("windowschips - Owner of EnergyBlade")
+crOdits:addButton("Krnl Development Team - Krnl Injector and API")
+crOdits:addButton("And you, "..game.Players.LocalPlayer.DisplayName.." - Supporting EnergyBlade")
